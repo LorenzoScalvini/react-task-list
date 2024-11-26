511dace5eb74288d3f7691b9bcee445c00a8c38f
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2024-11-26 17:22:25 +0100

    ADD: implementati i tag in base allo state della task

--- react-task-list/src/App.jsx
+++ react-task-list/src/App.jsx
@@ -8,42 +8,52 @@ function App() {
 
   return (
     <>
-      <div className='title'>
-        <h1>Task manager</h1>
-      </div>
+    <div className='title'>
+      <h1>Task manager</h1>
+    </div>
 
-      <div className='todo-lists'>
-        <div className='tasks-list'>
-          <h3 className='section-title'>Current tasks: </h3>
-          <ul>
-            {inProgressTasks.map(task => (
-              <li key={task.id}><strong className='task-title'>{task.title}</strong>
+    <div className='todo-lists'>
+      <div className='tasks-list'>
+        <h3 className='section-title'>Current tasks: </h3>
+        <ul>
+          {inProgressTasks.map(task => (
+            <li key={task.id}>
+              <strong className='task-title'>
+                {task.title}
+                <span className={`task-state ${task.state}`}>
+                  {task.state.charAt(0).toUpperCase() + task.state.slice(1)}
+                </span>
+              </strong>
               <ul>
                 <li>Priority: {task.priority}</li>
                 <li>Est time: {task.estimatedTime}</li>
               </ul>
-              </li>
-            ))}
-          </ul>
-        </div>
+            </li>
+          ))}
+        </ul>
+      </div>
 
-        <div className='tasks-list'>
-          <h3 className='section-title'>Completed tasks: </h3>
-          <ul>
-            {completedTasks.map(task => (
-              <li key={task.id}><strong className='task-title'>{task.title}</strong>
+      <div className='tasks-list'>
+        <h3 className='section-title'>Completed tasks: </h3>
+        <ul>
+          {completedTasks.map(task => (
+            <li key={task.id}>
+              <strong className='task-title'>
+                {task.title}
+                <span className={`task-state ${task.state}`}>
+                  {task.state.charAt(0).toUpperCase() + task.state.slice(1)}
+                </span>
+              </strong>
               <ul>
                 <li>Priority: {task.priority}</li>
                 <li>Est time: {task.estimatedTime}</li>
               </ul>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        
+            </li>
+          ))}
+        </ul>
       </div>
-    </>
+    </div>
+  </>
   )
 }
 
