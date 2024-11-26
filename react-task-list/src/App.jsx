b346eafb7172c6b9f40125cd1253203b3c6f8fee
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2024-11-26 15:45:14 +0100

    ADD: filter in-progress tasks

--- react-task-list/src/App.jsx
+++ react-task-list/src/App.jsx
@@ -3,20 +3,33 @@ import './App.css'
 import tasks from '../data/tasks'
 
 function App() {
-  const tasksToDo = tasks.filter(task => task.state === 'backlog' || task.state === 'in_progress');
-  
+    const inProgressTasks = tasks.filter(task => task.state === 'in_progress');
+    const completedTasks = tasks.filter(task => task.state === 'completed');
+
   return (
     <>
       <div className='title'>
         <h1>Task manager</h1>
       </div>
 
-      <div>
-        <h3>Current tasks</h3>
-      </div>
+      <div className='todo-lists'>
+        <div className='in-progress'>
+          <h3 className='section-title'>Current tasks</h3>
+          <ul>
+            {inProgressTasks.map(task => (
+              <li key={task.id}><strong className='task-title'>{task.title}</strong>
+              <ul>
+                <li>Priority: {task.priority}</li>
+                <li>Est time: {task.estimatedTime}</li>
+              </ul>
+              </li>
+            ))}
+          </ul>
+        </div>
 
-      <div>
-        <h3>Copleted tasks</h3>
+        <div>
+          <h3>Copleted tasks</h3>
+        </div>
       </div>
     </>
   )
